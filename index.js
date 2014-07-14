6f4395a569f91cc911f3473c5b1261e3b6e31aa8
Nicholas Kostelnik <nkostelnik@gmail.com>
2014-07-14 18:23:47 +0100

    console log the message

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var settings = require('env-settings'),
     request  = require('request');
 
 module.exports.message = function(message) {
+  console.log("Saying " + message);
   request.get(settings.say + "?say=" + message, function(err) {
     console.log("Failed to send say: " + message)
   })
